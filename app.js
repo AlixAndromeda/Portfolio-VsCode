64ee936859df0ce5185c3c78e606ef8493c4ff81
alialani <Ali_Al_Ani@outlook.com>
2022-07-28 23:32:44 +0100

    finit

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelector('.main-content');
-
-
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
@@ -13,21 +7,7 @@ const allSections = document.querySelector('.main-content');
             document.getElementById(button.dataset.id).classList.add("active");
         })
     });
-    //Sections Active
-    allSections.addEventListener('click', (e) => {
-        const id = e.target.dataset.id;
-        if(id) {
-            //resmove selected from the other button
-            sectBtns.forEach((btn) => {
-                btn.classList.remove('active')
-            })
-            e.target.classList.add('active')
-            //hide the other sections 
-            sections.forEach((section) => {
-                section.classList.remove('active')
-            })
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        } 
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
     })
 })();
